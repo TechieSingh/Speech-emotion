4dc55f0be853715325a1e127c4e115a75ee8a940
abhishekAtTheOneWorld <abhishek@theoneworld.in>
2022-07-06 01:33:26 -0700

    app

---
+++ speechemotionapp/lib/webview.dart
@@ -0,0 +1,174 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:webviewx/webviewx.dart';
+
+import 'helper.dart';
+
+
+class WebViewXPage extends StatefulWidget {
+  const WebViewXPage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  _WebViewXPageState createState() => _WebViewXPageState();
+}
+
+class _WebViewXPageState extends State<WebViewXPage> {
+  late WebViewXController webviewController;
+  final initialContent =
+      '<h4> This is some hardcoded HTML code embedded inside the webview <h4> <h2> Hello world! <h2>';
+  final executeJsErrorMessage =
+      'Failed to execute this task because the current content is (probably) URL that allows iframe embedding, on Web.\n\n'
+      'A short reason for this is that, when a normal URL is embedded in the iframe, you do not actually own that content so you cant call your custom functions\n'
+      '(read the documentation to find out why).';
+
+  Size get screenSize => MediaQuery.of(context).size;
+@override
+  void initState() {
+    // TODO: implement initState
+
+
+ 
+    super.initState();
+  }
+  @override
+  void dispose() {
+    webviewController.dispose();
+    super.dispose();
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Speech emotion  project'),
+      ),
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.all(10.0),
+          child: 
+       
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(width: 0.2),
+                ),
+                child: _buildWebViewX(),
+              ),
+             
+          
+        ),
+      ),
+    );
+  }
+
+  Widget _buildWebViewX() {
+    return WebViewX(
+
+      key: const ValueKey('webviewx'),
+      initialContent: initialContent,
+      initialSourceType: SourceType.html,
+      height: screenSize.height ,
+      width: screenSize.width ,
+      onWebViewCreated: (controller) {
+        
+        
+        
+        webviewController = controller;
+      webviewController.loadContent(
+      'https://share.streamlit.io/cybermaryver/speech-emotion-webapp/app.py',
+      SourceType.url,
+    );
+      },
+      onPageStarted: (src) =>
+          debugPrint('A new page has started loading: $src\n'),
+      onPageFinished: (src) =>
+          debugPrint('The page has finished loading: $src\n'),
+      jsContent: const {
+        EmbeddedJsContent(
+          js: "function testPlatformIndependentMethod() { console.log('Hi from JS') }",
+        ),
+        EmbeddedJsContent(
+          webJs:
+              "function testPlatformSpecificMethod(msg) { TestDartCallback('Web callback says: ' + msg) }",
+          mobileJs:
+              "function testPlatformSpecificMethod(msg) { TestDartCallback.postMessage('Mobile callback says: ' + msg) }",
+        ),
+      },
+      dartCallBacks: {
+        DartCallback(
+          name: 'TestDartCallback',
+          callBack: (msg) => showSnackBar(msg.toString(), context),
+        )
+      },
+      webSpecificParams: const WebSpecificParams(
+        printDebugInfo: true,
+      ),
+      mobileSpecificParams: const MobileSpecificParams(
+        androidEnableHybridComposition: true,
+      ),
+      navigationDelegate: (navigation) {
+        debugPrint(navigation.content.sourceType.toString());
+        return NavigationDecision.navigate;
+      },
+    );
+  }
+
+  void _setUrl() {
+    webviewController.loadContent(
+      'https://flutter.dev',
+      SourceType.url,
+    );
+  }
+
+ 
+
+
+
+ 
+
+  Future<void> _goForward() async {
+    if (await webviewController.canGoForward()) {
+      await webviewController.goForward();
+      showSnackBar('Did go forward', context);
+    } else {
+      showSnackBar('Cannot go forward', context);
+    }
+  }
+
+  Future<void> _goBack() async {
+    if (await webviewController.canGoBack()) {
+      await webviewController.goBack();
+      showSnackBar('Did go back', context);
+    } else {
+      showSnackBar('Cannot go back', context);
+    }
+  }
+
+  void _reload() {
+    webviewController.reload();
+  }
+
+
+  Widget buildSpace({
+    Axis direction = Axis.horizontal,
+    double amount = 0.2,
+    bool flex = true,
+  }) {
+    return flex
+        ? Flexible(
+            child: FractionallySizedBox(
+              widthFactor: direction == Axis.horizontal ? amount : null,
+              heightFactor: direction == Axis.vertical ? amount : null,
+            ),
+          )
+        : SizedBox(
+            width: direction == Axis.horizontal ? amount : null,
+            height: direction == Axis.vertical ? amount : null,
+          );
+  }
+
+
+}
